8ab0b782b347f29a7f48f3090fb7ec54088670d5
Your Name <you@example.com>
2026-02-10 12:47:52 +0100

    update

---
+++ .angular/cache/21.1.3/portfolio/vite/deps/@angular_cdk_portal.js
@@ -0,0 +1,24 @@
+import {
+  BasePortalOutlet,
+  CdkPortal,
+  CdkPortalOutlet,
+  ComponentPortal,
+  DomPortal,
+  DomPortalOutlet,
+  Portal,
+  PortalModule,
+  TemplatePortal
+} from "./chunk-5MVZMB2V.js";
+import "./chunk-H2IPCZHA.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BasePortalOutlet,
+  CdkPortal,
+  CdkPortalOutlet,
+  ComponentPortal,
+  DomPortal,
+  DomPortalOutlet,
+  Portal,
+  PortalModule,
+  TemplatePortal
+};
